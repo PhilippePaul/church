f734e7ea1fc30695ca2e994080109c4b28baacf5
PhilippePaul <philippe.paul@live.com>
2017-12-21 22:04:40 -0500

    WebServer include

---
+++ ChurchWebServer/ChurchWebServer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Net;
+
+namespace ChurchWebServer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopServer = false;
+            Task.Run(() =>
+            {
+                using (var httpListener = new HttpListener())
+                {
+                    httpListener.Prefixes.Add("http://localhost:8080/PortailEBETM");
+                    httpListener.Start();
+                    while (!stopServer)
+                    {
+                        httpListener.AuthenticationSchemes = AuthenticationSchemes.Basic;
+                        var context = httpListener.GetContext();
+                        Task.Run(() => HandleConnection(context));
+                    }
+                }
+            });
+
+            Console.ReadKey();
+            stopServer = true;
+        }
+
+        private static void HandleConnection(HttpListenerContext context)
+        {
+
+        }
+    }
+}
